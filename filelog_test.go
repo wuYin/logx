f23c3b7f313c81cffe353d86be2ad738cf715b5d
wuYin <wuYinPost@gmail.com>
2019-02-28 22:34:23 +0800

    自动备份测试通过

--- filelog_test.go
+++ filelog_test.go
@@ -1,18 +1,23 @@
 package logx
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 func TestFileLogWriter_LogWrite(t *testing.T) {
-	w := NewFileLogWriter("demo.log")
-	rec := &LogRecord{
-		Level:   ERROR,
-		Created: time.Now(),
-		Source:  "filelog_test.go",
-		Message: "test filelog write",
+	w := NewFileLogWriter("demo.log", 3, 10)
+
+	for i := 0; i < 4; i++ {
+		rec := &LogRecord{
+			Level:   ERROR,
+			Created: time.Now(),
+			Source:  "filelog_test.go",
+			Message: fmt.Sprintf("test backup: %d", i),
+		}
+		w.LogWrite(rec)
 	}
-	w.LogWrite(rec)
+
 	time.Sleep(1 * time.Second)
 }
