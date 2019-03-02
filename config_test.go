a075420da493bc2d0b475f0ed4a66a2b0e83e84a
wuYin <wuYinPost@gmail.com>
2019-03-02 09:07:02 +0800

    完善 config test

--- config_test.go
+++ config_test.go
@@ -1,11 +1,48 @@
 package logx
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
+var conf = `
+[
+    {
+        "enable": true,
+        "filter_type": "console",
+        "filter_name": "console_1",
+        "min_level": "INFO"
+    },
+    {
+        "enable": true,
+        "filter_type": "file",
+        "filter_name": "file_1",
+        "file_name": "warning.log",
+        "min_level": "WARN",
+        "format": "",
+        "max_line": "3",
+        "max_size": "2k"
+    }
+]
+`
+
 func TestLoadLogger(t *testing.T) {
-	logger, err := LoadLogger("conf.json")
+	f, err := ioutil.TempFile(".", "conf.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove(f.Name())
+	}()
+
+	n, err := f.WriteString(conf)
+	if err != nil || n != len(conf) {
+		t.Fatal(n, err)
+	}
+
+	logger, err := LoadLogger(f.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
