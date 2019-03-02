54951a194487ce5283f948a84e146936dced0062
wuYin <wuYinPost@gmail.com>
2019-03-02 21:37:27 +0800

    添加默认的 stdout logger

---
+++ default.go
@@ -0,0 +1,80 @@
+package logx
+
+import (
+	"fmt"
+	"strings"
+)
+
+var stdLogger = NewLogger()
+
+func init() {
+	stdLogger = &Logger{
+		"stdout": &Filter{DEBUG, NewConsoleLogWriter()},
+	}
+}
+
+func Fine(arg0 interface{}, args ...interface{}) {
+	level := FINE
+	switch v := arg0.(type) {
+	case string:
+		stdLogger.dispatch(level, v, args...)
+	default:
+		format := fmt.Sprint(arg0) + strings.Repeat(" %v", len(args))
+		stdLogger.dispatch(level, format, args...)
+	}
+}
+
+func Info(arg0 interface{}, args ...interface{}) {
+	level := INFO
+	switch v := arg0.(type) {
+	case string:
+		stdLogger.dispatch(level, v, args...)
+	default:
+		format := fmt.Sprint(arg0) + strings.Repeat(" %v", len(args))
+		stdLogger.dispatch(level, format, args...)
+	}
+}
+
+func Debug(arg0 interface{}, args ...interface{}) {
+	level := DEBUG
+	switch v := arg0.(type) {
+	case string:
+		stdLogger.dispatch(level, v, args...)
+	default:
+		format := fmt.Sprint(arg0) + strings.Repeat(" %v", len(args))
+		stdLogger.dispatch(level, format, args...)
+	}
+}
+
+func Warn(arg0 interface{}, args ...interface{}) {
+	level := WARN
+	switch v := arg0.(type) {
+	case string:
+		stdLogger.dispatch(level, v, args...)
+	default:
+		format := fmt.Sprint(arg0) + strings.Repeat(" %v", len(args))
+		stdLogger.dispatch(level, format, args...)
+	}
+}
+
+func Error(arg0 interface{}, args ...interface{}) {
+	level := ERROR
+	switch v := arg0.(type) {
+	case string:
+		stdLogger.dispatch(level, v, args...)
+	default:
+		format := fmt.Sprint(arg0) + strings.Repeat(" %v", len(args))
+		stdLogger.dispatch(level, format, args...)
+	}
+}
+
+func Fatal(arg0 interface{}, args ...interface{}) {
+	level := FATAL
+	switch v := arg0.(type) {
+	case string:
+		stdLogger.dispatch(level, v, args...)
+	default:
+		format := fmt.Sprint(arg0) + strings.Repeat(" %v", len(args))
+		stdLogger.dispatch(level, format, args...)
+	}
+}
