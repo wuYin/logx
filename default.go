ba183183bafddf6d87728255c248a5e32590ae4f
wuYin <wuYinPost@gmail.com>
2019-03-03 18:04:09 +0800

    issue: default 日志暂不可用

--- default.go
+++ default.go
@@ -14,7 +14,6 @@ func init() {
 }
 
 func Fine(arg0 interface{}, args ...interface{}) {
-	defer stdLogger.Close()
 	level := FINE
 	switch v := arg0.(type) {
 	case string:
@@ -26,7 +25,6 @@ func Fine(arg0 interface{}, args ...interface{}) {
 }
 
 func Info(arg0 interface{}, args ...interface{}) {
-	defer stdLogger.Close()
 	level := INFO
 	switch v := arg0.(type) {
 	case string:
@@ -38,7 +36,6 @@ func Info(arg0 interface{}, args ...interface{}) {
 }
 
 func Debug(arg0 interface{}, args ...interface{}) {
-	defer stdLogger.Close()
 	level := DEBUG
 	switch v := arg0.(type) {
 	case string:
@@ -50,7 +47,6 @@ func Debug(arg0 interface{}, args ...interface{}) {
 }
 
 func Warn(arg0 interface{}, args ...interface{}) {
-	defer stdLogger.Close()
 	level := WARN
 	switch v := arg0.(type) {
 	case string:
@@ -62,7 +58,6 @@ func Warn(arg0 interface{}, args ...interface{}) {
 }
 
 func Error(arg0 interface{}, args ...interface{}) {
-	defer stdLogger.Close()
 	level := ERROR
 	switch v := arg0.(type) {
 	case string:
@@ -74,7 +69,6 @@ func Error(arg0 interface{}, args ...interface{}) {
 }
 
 func Fatal(arg0 interface{}, args ...interface{}) {
-	defer stdLogger.Close()
 	level := FATAL
 	switch v := arg0.(type) {
 	case string:
